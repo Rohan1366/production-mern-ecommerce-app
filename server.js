7d3441433970aa7f9feccdd5dafc4901a452cd73
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-05-23 00:32:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path"
-import {fileURLToPath} from 'url'
+import {fileURLToPath} from'url'
 //configure env
 dotenv.config();
 
@@ -16,7 +16,7 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-cons __filename= fileURLToPath(import.meta.url)
+const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
 //rest object
 const app = express();
